d4767e5dd62254335450ebc572dcc65ac1a44a60
colinroche <colinr@DESKTOP-464BVEN.localdomain>
2023-06-27 14:22:57 +0100

    Adding comments to PlayerBaseState class

--- Assets/Scripts/Player/PlayerBaseState.cs
+++ Assets/Scripts/Player/PlayerBaseState.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public abstract class PlayerBaseState : State
 {
+    // variable for reference to the Player
     protected PlayerStateMachine stateMachine;
 
+    // constructor for passing in reference
     public PlayerBaseState(PlayerStateMachine stateMachine)
     {
         this.stateMachine = stateMachine;
