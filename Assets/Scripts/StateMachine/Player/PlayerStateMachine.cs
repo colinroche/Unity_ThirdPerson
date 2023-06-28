7eee2611ca248702456c59d000b903b7814a2c2c
colinroche <colinr@DESKTOP-464BVEN.localdomain>
2023-06-28 15:17:05 +0100

    Using input events to drive state machine

--- Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    // referencing something inside of a state
+    // get reference (make it a property)
+    // [field: SerializeField] - use for porperty so can be seen in Unity
+    [field: SerializeField] public InputReader InputReader { get; private set; }
 
     private void Start()
     {
--- Assets/Scripts/StateMachine/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachine/Player/PlayerTestState.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 
 public class PlayerTestState : PlayerBaseState
 {
-    private float timer = 5f;
+    private float timer;
     // constructor - as it is inheriting an abstract class
     public PlayerTestState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     // Implementing the abstract class
     public override void Enter()
     {
-        Debug.Log("Enter");
+        //subscribing to event call OnJump
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
 
     public override void Tick(float deltaTime)
     {
-        // decrementing timer
-        timer -= deltaTime;
+        timer += deltaTime;
 
         Debug.Log(timer);
-
-        // switching to new state every 5 seconds
-        if (timer <= 0f)
-        {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine));
-        } 
     }
 
     public override void Exit()
     {
-        Debug.Log("Exit");
+        // unsubscribing to event call OnJump
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine));
     }
 }
