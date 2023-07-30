47e08e3c7fffa1db6de642a0460d03891fd48c07
colinroche <colinr@DESKTOP-464BVEN.localdomain>
2023-07-30 18:12:27 +0100

    Improved Targeting Selection

--- Assets/Scripts/Combat/Targeting/Targeter.cs
+++ Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -9,12 +9,20 @@ public class Targeter : MonoBehaviour
 {
     [SerializeField] private CinemachineTargetGroup cineTargetGroup;
 
+    private Camera mainCamera;
+
     private List<Target> targets = new List<Target>();
     public Target CurrentTarget { get; private set; }
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.TryGetComponent<Target>(out Target target)) { return; }
+
         targets.Add(target);
         target.OnDestroyed += RemoveTarget;
     }
@@ -22,21 +30,48 @@ public class Targeter : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         if (!other.TryGetComponent<Target>(out Target target)) { return; }
+
         RemoveTarget(target);
     }
 
     public bool SelectTarget()
     {
-        // Select first target in list
         if (targets.Count == 0) { return false; }
-        CurrentTarget = targets[0];
+
+        Target closestTarget = null;
+        // Set to largest number possible so that it is larger then the distance to the first closest target
+        float closestTargetDistance = Mathf.Infinity;
+
+        // Loop over targets
+        foreach (Target target in targets)
+        {
+            // Check where target is on screen
+            Vector2 viewPos = mainCamera.WorldToViewportPoint(target.transform.position);
+
+            // Checking if target is on screen
+            if (viewPos.x < 0f || viewPos.x > 1f || viewPos.y < 0f || viewPos.y > 1f) { continue; }
+
+            // Setting the closet target to the current target if it is actually closer
+            Vector2 toCenter = viewPos - new Vector2(0.5f, 0.5f);
+            if (toCenter.sqrMagnitude < closestTargetDistance) 
+            {
+                closestTarget = target;
+                closestTargetDistance = toCenter.sqrMagnitude;
+            }
+        }
+
+        if (closestTarget == null) { return false; }
+
+        CurrentTarget = closestTarget;
         cineTargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
+
         return true;
     }
 
     public void Cancel()
     {
         if (CurrentTarget == null) { return; }
+
         cineTargetGroup.RemoveMember(CurrentTarget.transform);
         CurrentTarget = null;
     }
